bc8eb729b00222d76711451666d0605319ab9139
seunghan9 <106580567+seunghan9@users.noreply.github.com>
2022-09-26 23:18:06 +0900

    Unreal_Rule
    Unreal_Rule

---
+++ Source/TestUnrealEngine/MyActor.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+
+	RootComponent = Mesh;
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM(TEXT("StaticMesh'/Game/StarterContent/Props/SM_Couch.SM_Couch'"));
+
+	if (SM.Succeeded())
+	{
+		Mesh->SetStaticMesh(SM.Object);
+	}
+
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/TestUnrealEngine/MyActor.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+// 리플릭션 컴파일러가 읽는 주석이다.
+UCLASS()
+class TESTUNREALENGINE_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere, Category = BattleStat)
+	int32 HP;
+
+	UPROPERTY(EditAnywhere, Category = BattleStat)
+	int32 MP;
+};
